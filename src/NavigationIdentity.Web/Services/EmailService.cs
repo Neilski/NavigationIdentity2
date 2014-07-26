2fccef9575e114bc639f92556fbce6398b556bb9
Neil Martin <neil.martin@abilitation.com>
2014-07-26 15:56:05 +0100

    Email/SMS Configuration check added
    The Email and SMS service classes were modified to perform a  basic
    configuration check

--- src/NavigationIdentity.Web/Services/EmailService.cs
+++ src/NavigationIdentity.Web/Services/EmailService.cs
@@ -12,36 +12,45 @@ namespace NavigationIdentity.Web.Services
    {
       public Task SendAsync(IdentityMessage message)
       {
-         var fromEmailAddress = ConfigurationManager.AppSettings["IdentityFromEmailAddress"];
-
-         var text = message.Body;
-         var html = message.Body;
-
-         // Do whatever you want to the message
-         using (var msg = new MailMessage())
+         using (var smtpClient = new SmtpClient())
          {
-            msg.From = new MailAddress(fromEmailAddress);
-            msg.To.Add(new MailAddress(message.Destination));
-            msg.Subject = message.Subject;
-
-            msg.AlternateViews.Add(
-               AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain)
-            );
-
-            msg.AlternateViews.Add(
-               AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html)
-            );
-
-
-            // var smtpClient = new SmtpClient("smtp.whatever.net", Convert.ToInt32(587));
-            // var credentials = new System.Net.NetworkCredential(Keys.EmailUser, Keys.EMailKey);
-            // smtpClient.Credentials = credentials;
-
-            using (var smtpClient = new SmtpClient())
+            // Basic check for configured SMTP Service
+            if (String.IsNullOrWhiteSpace(smtpClient.Host))
             {
+               return Task.FromResult(0);
+            }
+
+            var fromEmailAddress =
+               ConfigurationManager.AppSettings["IdentityFromEmailAddress"];
+
+            var text = message.Body;
+            var html = message.Body;
+
+            // Do whatever you want to the message
+            using (var msg = new MailMessage())
+            {
+               msg.From = new MailAddress(fromEmailAddress);
+               msg.To.Add(new MailAddress(message.Destination));
+               msg.Subject = message.Subject;
+
+               msg.AlternateViews.Add(
+                  AlternateView.CreateAlternateViewFromString(
+                     text, null, MediaTypeNames.Text.Plain)
+                  );
+
+               msg.AlternateViews.Add(
+                  AlternateView.CreateAlternateViewFromString(
+                     html, null, MediaTypeNames.Text.Html)
+                  );
+
+               // var smtpClient = new SmtpClient("smtp.whatever.net", Convert.ToInt32(587));
+               // var credentials = new System.Net.NetworkCredential(Keys.EmailUser, Keys.EMailKey);
+               // smtpClient.Credentials = credentials;
+
                smtpClient.Send(msg);
             }
          }
+
          return Task.FromResult(0);
       }
 
--- src/NavigationIdentity.Web/Services/SmsService.cs
+++ src/NavigationIdentity.Web/Services/SmsService.cs
@@ -4,38 +4,48 @@ using System.Configuration;
 using System.Threading.Tasks;
 using Twilio;
 
+
 namespace NavigationIdentity.Web.Services
 {
    public class SmsService : IIdentityMessageService
    {
+
       public Task SendAsync(IdentityMessage message)
       {
+         // Plug in your sms service here to send a text message...
          // Grab the Twilio account details from Web.config
          var twilioSid = ConfigurationManager.AppSettings["IdentityTwilioSid"];
-         var twilioAuthToken = ConfigurationManager.AppSettings["IdentityTwilioAuthToken"];
-         var twilioNumber = ConfigurationManager.AppSettings["IdentityTwilioNumber"];
 
-         // Plug in your sms service here to send a text message...
-         var twilio = new TwilioRestClient(twilioSid, twilioAuthToken);
-         twilio.SendMessage(twilioNumber, message.Destination, message.Body);
+         if (!String.IsNullOrWhiteSpace(twilioSid))
+         {
+            var twilioAuthToken =
+               ConfigurationManager.AppSettings["IdentityTwilioAuthToken"];
+            var twilioNumber =
+               ConfigurationManager.AppSettings["IdentityTwilioNumber"];
+            var twilio = new TwilioRestClient(twilioSid, twilioAuthToken);
+            twilio.SendMessage(twilioNumber, message.Destination, message.Body);
+         }
 
          return Task.FromResult(0);
       }
+
 
       public static string CreateSecurityCodeMessage()
       {
          return String.Format(
             "Your {0} security code is: {{0}}",
             ApplicationService.ApplicationName
-         );
+            );
       }
+
 
       public static string CreateAuthenticatePhoneMessage(string code)
       {
          return String.Format(
             "Your {0} phone authentication security code is: {1}.",
             ApplicationService.ApplicationName, code
-         );
+            );
       }
+
    }
 }
